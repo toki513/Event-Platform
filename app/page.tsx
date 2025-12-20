94a40a9edcec0c64163717b2eae5720730d69f91
toki513 <tokitahmidkhan@gmail.com>
2025-12-21 00:01:52 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
     <ExploreBtn/>
 
     <div className='mt-20 space-y-7'>
-        <h3>Featured Events</h3>
+        <h3>Featured events</h3>
         <ul className='events'>
             {events.map((event) =>(
               <li key={event.title}>
